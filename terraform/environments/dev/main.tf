ab829cff754aab3046feb94c4dfc33460ea87748
Rohit Bhatt <rtigerbta33@gmail.com>
2026-06-22 14:11:19 +0530

    Add messaging module with SNS and SQS

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -3,6 +3,19 @@ module "storage" {
   source = "../../modules/storage"
 
   bucket_name = "cloud-platform-dev-state"
-  table_name= "cloud-platform-dev-lock"
+  table_name  = "cloud-platform-dev-lock"
 
+}
+module "secrets" {
+  source = "../../modules/secrets"
+
+  secret_name  = "database-password"
+  secret_value = "SuperSecret123"
+}
+
+module "messaging" {
+source = "../../modules/messaging"
+
+topic_name = "cloud-platform-events"
+queue_name = "cloud-platform-queue"
 }
--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  
+
   required_version = ">=1.5.0"
 
   required_providers {
-    aws={
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
@@ -13,19 +13,21 @@ terraform {
 provider "aws" {
   access_key = "test"
   secret_key = "test"
-  region = "us-east-1"
+  region     = "us-east-1"
 
   s3_use_path_style = true
 
   endpoints {
-    s3 = "http://localhost:4566"
-    dynamodb = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
     secretsmanager = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
   }
 
   skip_credentials_validation = true
-  skip_metadata_api_check = true
-  skip_requesting_account_id = true
-   
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
 
 }
